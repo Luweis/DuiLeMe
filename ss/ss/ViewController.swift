056a39c8b2bae51a82ffaf6fa7dde7c978419f56
wei lu <weilu@weidemacbook-pro.local>
2016-08-06 23:36:37 +0800

    test

---
+++ ss/ss/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ss
+//
+//  Created by wei lu on 16/8/6.
+//  Copyright © 2016年 wei lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("--- test----")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
